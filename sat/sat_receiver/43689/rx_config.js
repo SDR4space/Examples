e5acb25447e7329a13978a049a1ca63f31824419
Enzo <e.becamel@sdr4.space>
2021-07-16 23:10:02 +0200

    METOP-C

--- sat/sat_receiver/43689/rx_config.js
+++ sat/sat_receiver/43689/rx_config.js
@@ -1,6 +1,8 @@
 // 43689 METOP-C  465.9875 (ARGOS)
 var stream_name = '200k';
-var frequency= 465.9875;
+var frequency= 465.9575;
 var subband_bw= 22050;
-var offset= 0;
+var offset= 28.5e3;
 var nb_samples=10e6;
+var demod='M USB 2800';
+
