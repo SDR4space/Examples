d5aa52068e6a04df50dbecc46ed867de005a79f8
SDR4space <85399060+SDR4space@users.noreply.github.com>
2021-08-06 19:43:11 +0200

    Update spectrum.js

--- RX/spectrum/1_wide_spectrum/spectrum.js
+++ RX/spectrum/1_wide_spectrum/spectrum.js
@@ -1,31 +1,40 @@
+var fstart=88;
+var fend=108;
+// var samp_rate=4e6;   // PlutoSDR
+var samp_rate=2048e3;
+var fft=2048;
+
+
 IO.fdelete('/tmp/spectrum.csv');
-IO.fwritestr( '/tmp/spectrum.csv', '#nr,freq,spectrum,min_hold\n' );
-var rx = Soapy.makeDevice({'query' : 'driver=plutosdr' }) ;
+IO.fwritestr( '/tmp/spectrum.csv', '#nr,freq,spectrum\n' );
+
+// var rx = Soapy.makeDevice({'query' : 'driver=plutosdr' }) ;
+var rx = Soapy.makeDevice({'query' : 'driver=rtlsdr' }) ;
 rx.setGain( 65 );
-rx.setRxSampleRate(10e6);
+rx.setRxSampleRate(samp_rate);
 var value;
-for (var freq=704; freq < 946; freq += 7.5 ) {
-rx.setRxCenterFreq( freq );
 
 
 
-print("Freq : ",rx.getRxCenterFreq().toFixed(0)," MHz");
-//print("SR : ",rx.getRxSampleRate().toFixed(0));
+for (var freq=fstart+((samp_rate/1e6)*0.375); freq<fend+((samp_rate/1e6)*0.375); freq+=((samp_rate/1e6)*0.75) ) {
 
-var IQ = rx.Capture( 1000000 );
-var spectrum = IQ.getPowerSpectrum( 1024 ) ; 
-//peaks = spectrum.peaks ;
-//print('Peaks object:' + JSON.stringify( peaks ));
-//print(JSON.stringify(spectrum));
-//print(spectrum.spectrum.length);
+	rx.setRxCenterFreq( freq );
+	print("Freq : ",freq.toFixed(3)," MHz");
 
 
-for (var a=128 ; a < spectrum.spectrum.length-128; a++) {
-	value += JSON.stringify(spectrum.frequencies[a]) + ',' + JSON.stringify(spectrum.spectrum[a]) + '\n';
-	}
+	var IQ = rx.Capture( 500000 );
+	var spectrum = IQ.getPowerSpectrum( fft ) ; 
+
+	//print(JSON.stringify(spectrum));
 
 
+	for (var a=fft/8 ; a < spectrum.spectrum.length-(fft/8); a++) {
+	        if ((spectrum.frequencies[a] >fstart) && (spectrum.frequencies[a]<fend)) {
+		value += JSON.stringify(spectrum.frequencies[a]) + ',' + JSON.stringify(spectrum.spectrum[a]) + '\n';
+		}
+        };
 }
+
 IO.fappend('/tmp/spectrum.csv',value);
 var c = {
     'command' : '/usr/bin/gnuplot-x11',
